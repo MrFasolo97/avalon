815009e5b7fa297268f9b9ba58ac337cdc65c01c
Andy Zehady <azehady@cisco.com>
2021-08-26 21:04:36 -0500

    Code update 33

--- src/http/content/index.js
+++ src/http/content/index.js
@@ -2,6 +2,57 @@ const parallel = require('run-parallel')
 
 module.exports = {
     init: (app) => {
+        // get new contents
+        app.get('/content/:author/:link', (req, res) => {
+            if (!req.params.author || typeof req.params.link !== 'string') {
+                res.sendStatus(500)
+                return
+            }
+            db.collection('contents').findOne({
+                author: req.params.author,
+                link: req.params.link
+            }, function (err, post) {
+                if (!post) {
+                    res.sendStatus(404)
+                    return
+                }
+                if (!post.child || post.child.length === 0) {
+                    res.send(post)
+                    return
+                }
+                post.comments = {}
+                function fillComments(posts, cb) {
+                    if (!posts || posts.length === 0) {
+                        cb()
+                        return
+                    }
+                    var executions = []
+                    for (let i = 0; i < posts.length; i++)
+                        executions.push(function (callback) {
+                            db.collection('contents').find({
+                                pa: posts[i].author,
+                                pp: posts[i].link
+                            }).toArray(function (err, comments) {
+                                for (let y = 0; y < comments.length; y++)
+                                    post.comments[comments[y].author + '/' + comments[y].link] = comments[y]
+                                fillComments(comments, function () {
+                                    callback(null, true)
+                                })
+                            })
+                            i++
+                        })
+
+                    parallel(executions, function (err, results) {
+                        if (err) throw err
+                        cb(null, results)
+                    })
+                }
+                fillComments([post], function () {
+                    res.send(post)
+                })
+            })
+        })
+
         // get content by tag with limit by certain author
         // filter = author,tag,limit,ts(from, to)
         // $API_URL/filter?author=author1,author2,...,authorN&tag=tag1,tag2,...,tagN&limit=x&ts=tsfrom-tsto
@@ -183,56 +234,6 @@ module.exports = {
                     res.send(contents)
                 })
             }
-        })
-        // get new contents
-        app.get('/content/:author/:link', (req, res) => {
-            if (!req.params.author || typeof req.params.link !== 'string') {
-                res.sendStatus(500)
-                return
-            }
-            db.collection('contents').findOne({
-                author: req.params.author,
-                link: req.params.link
-            }, function (err, post) {
-                if (!post) {
-                    res.sendStatus(404)
-                    return
-                }
-                if (!post.child || post.child.length === 0) {
-                    res.send(post)
-                    return
-                }
-                post.comments = {}
-                function fillComments(posts, cb) {
-                    if (!posts || posts.length === 0) {
-                        cb()
-                        return
-                    }
-                    var executions = []
-                    for (let i = 0; i < posts.length; i++)
-                        executions.push(function (callback) {
-                            db.collection('contents').find({
-                                pa: posts[i].author,
-                                pp: posts[i].link
-                            }).toArray(function (err, comments) {
-                                for (let y = 0; y < comments.length; y++)
-                                    post.comments[comments[y].author + '/' + comments[y].link] = comments[y]
-                                fillComments(comments, function () {
-                                    callback(null, true)
-                                })
-                            })
-                            i++
-                        })
-
-                    parallel(executions, function (err, results) {
-                        if (err) throw err
-                        cb(null, results)
-                    })
-                }
-                fillComments([post], function () {
-                    res.send(post)
-                })
-            })
         })
     }
 }

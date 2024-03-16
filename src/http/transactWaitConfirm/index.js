78c20c7f213c90c2664e3b3e30621386279133e4
MrFasolo97 <79fasolo97@gmail.com>
2024-03-17 00:07:57 +0100

    replace http 500 with a 307 redirect

--- src/http/transactWaitConfirm/index.js
+++ src/http/transactWaitConfirm/index.js
@@ -3,8 +3,7 @@ const timeout_transact_async = 7500
 module.exports = {
     init: (app) => {
         app.post('/transactWaitConfirm', (req, res) => {
-            let tx = req.body
-            res.status(500).send({ error: "Endpoint removed" })
+            res.redirect(307, "/transact")
         })
     }
 }

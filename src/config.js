29870521bb606179b4043cf5b7387c8fc385633c
MrFasolo97 <79fasolo97@gmail.com>
2024-03-25 13:26:37 +0100

    Change genesis hash, new testnet

--- src/config.js
+++ src/config.js
@@ -95,7 +95,7 @@ let config = {
             // the maximum number of mentions triggering a notification
             notifMaxMentions: 10,
             // the sha256sum hash of block 0 (new origin hash -> new chain)
-            originHash: 'da5fe18d0844f1f97bf5a94e7780dec18b4ab015e32383ede77158e059bacbb2',
+            originHash: 'e378784bfb9cddb4e1bbd364b044bf499a05846a163f4961f680659b2f54c4a8',
             // the default number of random bytes to use for new key generation
             randomBytesLength: 32,
             // the maximum share of the reward pool a single distribution can generate

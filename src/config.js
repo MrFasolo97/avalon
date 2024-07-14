304a54f60bd1f23f9c4818e7fa0b39e54c9ca10d
MrFasolo97 <79fasolo97@gmail.com>
2024-07-15 00:27:07 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -96,7 +96,7 @@ let config = {
             // the maximum number of mentions triggering a notification
             notifMaxMentions: 10,
             // the sha256sum hash of block 0 (new origin hash -> new chain)
-            originHash: '6a008414400452494d46750fa47d1025080b8efdf1665e94d28bff508b95e5c4',
+            originHash: '64f1cb98078e28c29f4e1a89a6ba6e1415ffffb4a12d76328945803552fd00f2',
             // the default number of random bytes to use for new key generation
             randomBytesLength: 32,
             // the maximum share of the reward pool a single distribution can generate

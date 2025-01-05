9f29f3e02d7c83d6cd404fb2f2bd19e9484344a0
MrFasolo97 <79fasolo97@gmail.com>
2025-01-05 01:44:24 +0100

    Fix

--- src/transactions/cleanStaleNodes.js
+++ src/transactions/cleanStaleNodes.js
@@ -2,7 +2,7 @@ module.exports = {
     fields: ["memo"],
     validate: (tx, ts, legitUser, cb) => {
         try {
-            let leaders = cache.findMany("accounts", { node_appr: { $gt: 0 }}).sort({ node_appr: -1, name: -1 }).limit(config.leaders).toArray();
+            let leaders = cache.findMany("leaders", { node_appr: { $gt: 0 }}).sort({ node_appr: -1, name: -1 }).limit(config.leaders).toArray();
             for (let leader in leaders)
                 if (leaders[leader].name == tx.sender)
                     cb(true);
@@ -15,7 +15,7 @@ module.exports = {
     },
     execute: (tx, ts, cb) => {
         try {
-        let leaders = cache.findMany("accounts", { node_appr: { $gt: 0 }}).sort({node_appr: -1, name: -1}).limit(config.leaders).toArray();
+        let leaders = cache.findMany("leaders", { node_appr: { $gt: 0 }}).sort({node_appr: -1, name: -1}).limit(config.leaders).toArray();
             for (let i in leaders) {
                 try {
                     let leader = cache.findOne("leaders").find({_id: leaders[i].name})

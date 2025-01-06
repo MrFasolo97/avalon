daa5c4f9a93caa5f165262efb6593c416295408d
MrFasolo97 <79fasolo97@gmail.com>
2025-01-06 20:57:58 +0100

    Update cleanStaleNodes.js

--- src/transactions/cleanStaleNodes.js
+++ src/transactions/cleanStaleNodes.js
@@ -4,7 +4,7 @@ module.exports = {
         try {
             cache.findMany("leaders", { node_appr: { $gt: 0 }}, { node_appr: -1, _id: -1 }, (err, leaders) => {
                 if (err) throw err;
-                for (let leader=0; leader<config.leaders; i++)
+                for (let leader=0; leader<config.leaders && leader<leaders.length; i++)
                     if (leaders[leader]._id == tx.sender)
                         cb(true);
                 cb(false, "Unauthorized sender");

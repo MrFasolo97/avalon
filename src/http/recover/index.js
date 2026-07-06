60fc482ceae30dcdcf63ef8681af1e6b81dbce24
MrFasolo97 <79fasolo97@gmail.com>
2026-07-06 18:07:23 +0200

    fix: add optional auth and audit logging to /recover endpoint

--- src/http/recover/index.js
+++ src/http/recover/index.js
@@ -1,6 +1,13 @@
 module.exports = {
     init: (app) => {
         app.get('/recover',(req,res) => {
+            const requiredToken = process.env.RECOVER_TOKEN
+            if (requiredToken) {
+                const bearer = req.headers['authorization'] ? req.headers['authorization'].replace('Bearer ', '') : ''
+                if (bearer !== requiredToken && req.query.token !== requiredToken)
+                    return res.status(401).send({error: 'invalid token'})
+            }
+            logr.warn('Manual recovery triggered from ' + req.ip)
             p2p.refresh(true)
             res.send({})
         })

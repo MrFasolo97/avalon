fcbf719c6ccd7c39749c4432e5119f45a3c98caa
MrFasolo97 <79fasolo97@gmail.com>
2026-08-23 23:51:41 +0200

    fix(http): harden leader endpoint against missing fields and errors

--- src/http/leader/index.js
+++ src/http/leader/index.js
@@ -26,7 +26,7 @@ module.exports = {
             if (!req.params.account)
                 res.status(404).send({error: 'account is required'})
             db.collection('accounts').findOne({name: req.params.account}, (e,acc) => {
-                if (e) return res.status(500).send(e)
+                if (e) { logr.error('leader query failed', e); return res.status(500).send({error: 'query failed'}) }
                 if (!acc) return res.status(404).send({error: 'account does not exist'})
                 if (!leaderStats.leaders[acc.name] || (!acc.pub_leader && !leaderStats.leaders[acc.name].last))
                     return res.status(404).send({error: 'account does not have any leader-related activities'})
@@ -35,8 +35,8 @@ module.exports = {
                     balance: acc.balance,
                     node_appr: acc.node_appr,
                     pub_leader: acc.pub_leader,
-                    subs: acc.followers.length,
-                    subbed: acc.follows.length,
+                    subs: acc.followers ? acc.followers.length : 0,
+                    subbed: acc.follows ? acc.follows.length : 0,
                     sinceTs: leaderStats.leaders[acc.name].sinceTs,
                     sinceBlock: leaderStats.leaders[acc.name].sinceBlock,
                     produced: leaderStats.leaders[acc.name].produced,
